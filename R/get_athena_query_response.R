76ea34562bcc4d41d7bd772f23ff65bb03c9862a
Karik Isichei <karik.isichei@justice.gsi.gov.uk>
2018-10-12 11:31:02 +0000

    should return better error on Athena failure

--- R/get_athena_query_response.R
+++ R/get_athena_query_response.R
@@ -12,6 +12,8 @@
 #'
 #'@param return_athena_types Specifies if the list describing the data's meta data types should be defined using athena datatypes (TRUE) or using the data engineering team's generic metadata types (FALSE). If not specified the default value of this input parameter is set to FALSE.
 #'
+#'@param timeout Specifies How long you want your sql query to wait before it gives up (in seconds). Default parameter is NULL which will mean SQL query will not timeout and could wait forever if an issue occured.
+#'
 #'@return A list with two keys. [1] s3_path : a string pointing to the s3 path of the athena query. [2] meta : a list that has the name and type of each column in of the data in the s3_path. Can be used to get correct data types of your output when read in to R (note the order of the columns matches the order they appear in the data).
 #'
 #'@examples
@@ -28,7 +30,7 @@
 #'s3_path_stripped = gsub("s3://", "", response$s3_path)
 #'df <- s3tools::read_using(FUN = readr::read_csv, s3_path=s3_path_stripped)
 
-get_athena_query_response <- function(sql_query, return_athena_types=FALSE){
+get_athena_query_response <- function(sql_query, return_athena_types=FALSE, timeout = NULL){
 
   # Annoyingly I think you have to pull it in as the source_python function doesn't seem to be exported properly
   require(reticulate)
@@ -36,6 +38,6 @@ get_athena_query_response <- function(sql_query, return_athena_types=FALSE){
   python_script <- system.file("extdata", "get_athena_query_response.py", package = "dbtools")
   reticulate::source_python(python_script)
   s3tools::get_credentials()
-  response <- get_athena_query_response(sql_query=sql_query, return_athena_types=return_athena_types)
+  response <- get_athena_query_response(sql_query=sql_query, return_athena_types=return_athena_types, timeout=timeout)
   return(response)
 }
--- inst/extdata/get_athena_query_response.py
+++ inst/extdata/get_athena_query_response.py
@@ -1,10 +1,10 @@
-import boto3 
+import boto3
 import pandas as pd
 import io
-import time 
+import time
 
-def get_athena_query_response(sql_query, return_athena_types = False) :
-  
+def get_athena_query_response(sql_query, return_athena_types = False, timeout = None) :
+
     type_dictionary = {
         "char" : "character",
         "varchar" : "character",
@@ -16,7 +16,7 @@ def get_athena_query_response(sql_query, return_athena_types = False) :
         "float" : "float",
         "double" : "double"
     }
-  
+
     def s3_path_to_bucket_key(s3_path):
         """
         Splits out s3 file path to bucket key combination
@@ -36,9 +36,8 @@ def get_athena_query_response(sql_query, return_athena_types = False) :
             'OutputLocation': temp_folder,
       }
     )
-    
+
     sleep_time = 2
-    timeout = 60
     counter = 0
     while True :
         athena_status = athena_client.get_query_execution(QueryExecutionId = response['QueryExecutionId'])
@@ -47,19 +46,22 @@ def get_athena_query_response(sql_query, return_athena_types = False) :
         elif athena_status['QueryExecution']['Status']['State'] in ['QUEUED','RUNNING'] :
             # print('waiting...')
             time.sleep(sleep_time)
+        elif athena_status['QueryExecution']['Status']['State'] == 'FAILED' :
+            raise ValueError("athena failed - response error:\n {}".format(athena_status['QueryExecution']['Status']['StateChangeReason']))
         else :
-            raise ValueError('athena failed')
+            raise ValueError("athena failed - unknown reason (printing full response):\n {athena_status}".format(athena_status))
+
         counter += 1
-        
-        if counter*sleep_time > timeout :
-            raise ValueError('athena timed out')
-    
+        if timeout :
+          if counter*sleep_time > timeout :
+              raise ValueError('athena timed out')
+
     result_response = athena_client.get_query_results(QueryExecutionId=athena_status['QueryExecution']['QueryExecutionId'], MaxResults=1)
     s3_path = athena_status['QueryExecution']['ResultConfiguration']['OutputLocation']
     if return_athena_types :
         meta = [{'name':c['Name'], 'type' : c['Type']} for c in result_response['ResultSet']['ResultSetMetadata']['ColumnInfo']]
     else :
         meta = [{'name':c['Name'], 'type' : type_dictionary[c['Type']]} for c in result_response['ResultSet']['ResultSetMetadata']['ColumnInfo']]
-    
+
     return {'s3_path' : s3_path, 'meta' : meta}
 
