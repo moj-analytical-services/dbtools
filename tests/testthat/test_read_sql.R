d960c22ce2b02ac1744f62b1d02cb46d6037c710
Karik Isichei <karik.isichei@justice.gsi.gov.uk>
2019-01-10 16:57:59 +0000

    done for today, need to fix boto3 refresh creds not working in reticulate

--- tests/testthat/test_read_sql.R
+++ tests/testthat/test_read_sql.R
@@ -1,8 +1,10 @@
 context("Test data reads in with correct meta data")
 
-test_that("Test read_sql", {
+test_that("data from read_sql conforms", {
 
   tib <- dbtools::read_sql("SELECT * from dbtools.test_data", 'dbtools-test-bucket', return_df_as='tibble')
+  names(tib)
+
   df <- dbtools::read_sql("SELECT * from dbtools.test_data", 'dbtools-test-bucket', return_df_as='dataframe')
   dt <- dbtools::read_sql("SELECT * from dbtools.test_data", 'dbtools-test-bucket', return_df_as='data.table')
 
