8d59cb1b401d3427825255cc8f876ccc3ef4642a
Karik Isichei <karik.isichei@justice.gsi.gov.uk>
2019-01-11 12:59:17 +0000

    saving progress, almost finished UTs for tibble read_sql

--- tests/testthat/test_read_sql.R
+++ tests/testthat/test_read_sql.R
@@ -1,10 +1,30 @@
 context("Test data reads in with correct meta data")
 
-test_that("data from read_sql conforms", {
+test_that("data from read_sql as tibble conforms", {
 
-  tib <- dbtools::read_sql("SELECT * from dbtools.test_data", 'dbtools-test-bucket', return_df_as='tibble')
-  names(tib)
+  # Test all cols read
+  tib <- dbtools::read_sql("SELECT * from dbtools.test_data", 'alpha-dbtools-test-bucket', return_df_as='tibble')
+  t1 <- all(colnames(tib) == c("character_col","int_col","long_col","date_col","datetime_col","boolean_col","float_col","double_col"))
+  expect_true(t1)
 
+  # Test all rows read
+  t2 <- length(tib) == 8
+  expect_true(t2)
+
+  # Test col types
+  type_vec <- c()
+  class_vec <- c()
+  for (c in colnames(tib)){
+    type_vec <- c(type_vec, typeof(tib[[c]]))
+    class_vec <- c(class_vec, paste(class(tib[[c]]), collapse = ' '))
+  }
+
+  t3 <- all(type_vec ==  c("character","integer","integer","double","double","logical","double","double"))
+  t4 <- all(class_vec == c("character","integer","integer","Date","POSIXct POSIXt","logical","numeric","numeric"))
+  expect_true(t3)
+  expect_true(t4)
+
+  # Test col types
   df <- dbtools::read_sql("SELECT * from dbtools.test_data", 'dbtools-test-bucket', return_df_as='dataframe')
   dt <- dbtools::read_sql("SELECT * from dbtools.test_data", 'dbtools-test-bucket', return_df_as='data.table')
 
