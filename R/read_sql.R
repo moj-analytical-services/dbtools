216b5b0aecdf6873dffa4617631d27e139cc19d6
Karik Isichei <karik.isichei@justice.gsi.gov.uk>
2019-06-14 11:49:49 +0000

    Added same functionality as pydbtools.read_sql where you can now read text outputs from Athena. E.g. SHOW COLUMNS FROM db.table

--- R/read_sql.R
+++ R/read_sql.R
@@ -16,7 +16,7 @@
 #'
 #'@param timeout Specifies How long you want your sql query to wait before it gives up (in seconds). Default parameter is NULL which will mean SQL query will not timeout and could wait forever if an issue occured.
 #'
-#'@return A table as a Dataframe, tibble or data.table
+#'@return A table as a dataframe, tibble or data.table
 #'
 #'@examples
 #'# Read an sql query using readr::read_csv i.e. returning a Tibble
@@ -38,28 +38,49 @@ read_sql <- function(sql_query, return_df_as='tibble', timeout = NULL){
   bucket <- unlist(strsplit(s3_path_stripped, '/'))[1]
   s3_key <- gsub(paste0(bucket,"/"), "", s3_path_stripped)
 
-  data_conversion <- dbtools:::get_data_conversion(return_df_as)
-  col_classes = list()
+  # Check if text output
+  if(endsWith(s3_path_stripped, ".txt")){
+    if(return_df_as == 'tibble'){
+      df <- s3tools::read_using(FUN=readr::read_csv, s3_path=s3_path_stripped, col_names=FALSE, col_types=list(readr::col_character()))
+    } else if(return_df_as == 'data.table'){
+      df <- s3tools::read_using(FUN=data.table::fread, s3_path=s3_path_stripped, header=FALSE, colClasses=c("character"))
+    } else {
+      df <- s3tools::read_using(FUN=read.csv, s3_path=s3_path_stripped, header=FALSE, colClasses=c("character"))
+    }
+    colnames(df) <- c("output")
+  } else {
+    # Get meta Ddata conversion
+    data_conversion <- dbtools:::get_data_conversion(return_df_as)
+    col_classes = list()
     for(m in response$meta){
       col_classes[[m$name]] = data_conversion[[m$type]]
     }
-  col_classes_vec = unlist(col_classes)
+    col_classes_vec = unlist(col_classes)
 
-  if(return_df_as == 'tibble'){
-    #This is the best R work arround I could find to replicate Python's **kwargs...
-    col_types = do.call(readr::cols, col_classes)
-    df <- s3tools::read_using(FUN=readr::read_csv, s3_path=s3_path_stripped, col_names=TRUE, col_types=col_types)
+    if(return_df_as == 'tibble'){
+      #This is the best R work arround I could find to replicate Python's **kwargs...
+      col_types = do.call(readr::cols, col_classes)
+      df <- s3tools::read_using(FUN=readr::read_csv, s3_path=s3_path_stripped, col_names=TRUE, col_types=col_types)
 
-  } else if(return_df_as == 'data.table'){
-    dt_ver <- packageVersion("data.table")
-    if(dt_ver < '1.11.8'){
-      warning("Your version of data.table must be 1.11.8 or above please install a new version otherwise your outputs of type data.table may not convert data types properly.")
+    } else if(return_df_as == 'data.table'){
+      dt_ver <- packageVersion("data.table")
+      if(dt_ver < '1.11.8'){
+        warning("Your version of data.table must be 1.11.8 or above please install a new version otherwise your outputs of type data.table may not convert data types properly.")
+      }
+      df <- s3tools::read_using(FUN=data.table::fread, s3_path=s3_path_stripped, header=TRUE, colClasses=col_classes_vec)
+    } else {
+      df <- s3tools::read_using(FUN=read.csv, s3_path=s3_path_stripped, header=TRUE, colClasses=col_classes_vec)
     }
-    df <- s3tools::read_using(FUN=data.table::fread, s3_path=s3_path_stripped, header=TRUE, colClasses=col_classes_vec)
-  } else {
-    df <- s3tools::read_using(FUN=read.csv, s3_path=s3_path_stripped, header=TRUE, colClasses=col_classes_vec)
   }
   dbtools:::delete_object(bucket, s3_key)
   dbtools:::delete_object(bucket, paste0(s3_key, ".metadata"))
   return(df)
 }
+
+df1 = read.csv('test.txt', header = F)
+colnames(df1) <- c("output")
+df2 = readr::read_csv('test.txt', col_names = F)
+colnames(df2) <- c("output")
+df3 = data.table::fread('test.txt', header = F)
+colnames(df3) <- c("output")
+
