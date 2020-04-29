d2885fe96f171c0db8916f69a574061b24979efd
Thomas-Hirsch <37899506+Thomas-Hirsch@users.noreply.github.com>
2020-04-29 14:04:26 +0100

    specify envname

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   if(!reticulate::py_module_available("pydbtools")) {
-    reticulate::conda_install(packages = "pydbtools", pip = TRUE)
+    reticulate::conda_install(envname = "rstudio", packages = "pydbtools", pip = TRUE)
   }
 }
