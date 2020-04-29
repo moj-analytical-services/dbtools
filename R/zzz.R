cda07f37eac39b61129f004974c483c7e8035e23
Thomas-Hirsch <37899506+Thomas-Hirsch@users.noreply.github.com>
2020-04-29 13:28:46 +0100

    typo

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  if(!reticulate::py_module_available("pydbtools") {
+  if(!reticulate::py_module_available("pydbtools")) {
     reticulate::conda_install(packages = "pydbtools", pip = TRUE)
   }
 }
