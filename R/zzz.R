23a5bbf56aa0e2df056d460d39b5f5009f0c71c1
Thomas-Hirsch <37899506+Thomas-Hirsch@users.noreply.github.com>
2020-04-30 16:13:13 +0100

    specify version

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   if(!reticulate::py_module_available("pydbtools")) {
-    reticulate::conda_install(envname = "rstudio", packages = "pydbtools=1.0.3", pip = TRUE)
+    reticulate::conda_install(envname = "rstudio", packages = "pydbtools==1.0.3", pip = TRUE)
   }
 }
