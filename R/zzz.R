4d9e08fc5e2bf2446805e59c58b2612217a4e8ad
mralecjohnson <alec.johnson@digita.justice.gov.uk>
2020-09-21 10:44:53 +0000

    select python installation before running

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onLoad <- function(libname, pkgname) {
+  reticulate::use_python(".conda/envs/rstudio/bin/python")
   if(!reticulate::py_module_available("pydbtools")) {
     reticulate::conda_install(envname = "rstudio", packages = "pydbtools==2.0.0", pip = TRUE)
   }
