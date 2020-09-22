779be377f256887ea9adef4b4873de7f6590a0b8
mralecjohnson <alec.johnson@digita.justice.gov.uk>
2020-09-22 16:07:37 +0000

    Couple of comments in zzz file

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
 .onLoad <- function(libname, pkgname) {
+  # Construct Python path and pass it to reticulate
   base_path <- strsplit(Sys.getenv("PATH"), ":")[[1]][1]
   python_path <- paste(base_path, "/python", sep = "")
   reticulate::use_python(python_path)
+
+  # Check if pydbtools is installed. If it isn't, install it through Conda
   if(!reticulate::py_module_available("pydbtools")) {
     reticulate::conda_install(envname = "rstudio", packages = "pydbtools==2.0.1", pip = TRUE)
   }
