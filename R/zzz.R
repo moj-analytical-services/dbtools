10147ec9df0d88f3da7316cb645b210bcf979dd7
mralecjohnson <alec.johnson@digita.justice.gov.uk>
2020-09-22 15:28:16 +0000

    Foolish typo and switched to proper R assignment syntax

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
-  base_path = strsplit(Sys.getenv("PATH"), ":")[[1]][1]
-  python_bath = paste(base_path, "/python", sep = "")
+  base_path <- strsplit(Sys.getenv("PATH"), ":")[[1]][1]
+  python_path <- paste(base_path, "/python", sep = "")
   reticulate::use_python(python_path)
   if(!reticulate::py_module_available("pydbtools")) {
     reticulate::conda_install(envname = "rstudio", packages = "pydbtools==2.0.1", pip = TRUE)
