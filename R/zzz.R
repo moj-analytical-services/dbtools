a9314056680ba0b18454cb03ebcc64437d445732
Mike Ratford <mike.ratford@gmail.com>
2022-02-15 13:52:14 +0000

    Remove load of python function

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,3 @@
-reticulate::source_python(
-  system.file("python", "read_sql_query_py.py", package = "dbtools"))
-
 dbtools.env <- new.env(parent=emptyenv())
 .onLoad <- function(libname, pkgname) {
   # import Python package on package load
